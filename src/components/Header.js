bd32873c92914ca3da596ac1905b930a662e9136
Surykant <kasare9096464112@gmail.com>
2022-05-13 16:39:32 +0530

    secondcommit

--- src/components/Header.js
+++ src/components/Header.js
@@ -18,45 +18,45 @@ function Header() {
               <li className="dropdown"><a href="/"><span>Standards</span> 
               <i className="bi bi-chevron-down" /></a>
                 <ul>
-                  <li><a href="/">Class 5</a></li>
+                  <li><a href="">Class 5</a></li>
                   <li className="dropdown"><a href="/"><span>&nbsp;&nbsp;&nbsp;&nbsp; Subject</span> <i className="bi bi-chevron-right" /></a>
                     <ul>
-                      <li><a href="http://ktbs.kar.nic.in/New/website%20textbooks/class9/9th%20standard/9th-english-maths-1.pdf">Math</a></li>
+                      <li><a href="https://jobscaptain.com/basic-maths-formulas-pdf/">Math</a></li>
                       <li><a href="/">Science</a></li>
                     </ul>
                   </li>
-                  <li><a href="/">Class 6</a></li>
+                  <li><a href="">Class 6</a></li>
                   <li className="dropdown"><a href="/"><span>&nbsp;&nbsp;&nbsp;&nbsp; Subject</span> <i className="bi bi-chevron-right" /></a>
                     <ul>
-                      <li><a href="/">Math</a></li>
+                      <li><a href="http://sarkariresult.schools360.in/download/maths-formulas-for-class-6-pdf/">Math</a></li>
                       <li><a href="/">Science</a></li>
                     </ul>
                   </li>
                   <li><a href="/">Class 7</a></li>
                   <li className="dropdown"><a href="/"><span>&nbsp;&nbsp;&nbsp;&nbsp; Subject</span> <i className="bi bi-chevron-right" /></a>
                     <ul>
-                      <li><a href="/">Math</a></li>
+                      <li><a href="http://sarkariresult.schools360.in/download/maths-formulas-for-class-8-pdf/">Math</a></li>
                       <li><a href="/">Science</a></li>
                     </ul>
                   </li>
                   <li><a href="/">Class 8</a></li>
                   <li className="dropdown"><a href="/"><span>&nbsp;&nbsp;&nbsp;&nbsp; Subject</span> <i className="bi bi-chevron-right" /></a>
                     <ul>
-                      <li><a href="/">Math</a></li>
+                      <li><a href="http://sarkariresult.schools360.in/download/maths-formulas-for-class-8-pdf/">Math</a></li>
                       <li><a href="/">Science</a></li>
                     </ul>
                   </li>
                   <li><a href="/">Class 9</a></li>
                   <li className="dropdown"><a href="/"><span>&nbsp;&nbsp;&nbsp;&nbsp; Subject</span> <i className="bi bi-chevron-right" /></a>
                     <ul>
-                      <li><a href="/">Math</a></li>
-                      <li><a href="/">Science</a></li>
+                      <li><a href="https://drive.google.com/file/d/1qmbfLtIVixo2jRvyCpWRjoHjoScsnKst/view?usp=sharing">Math</a></li>
+                      <li><a href="http://ktbs.kar.nic.in/New/website%20textbooks/class9/9th%20standard/9th-english-maths-1.pdf">Science</a></li>
                     </ul>
                   </li>
                   <li><a href="/">Class 10</a></li>
                   <li className="dropdown"><a href="/"><span>&nbsp;&nbsp;&nbsp;&nbsp; Subject</span> <i className="bi bi-chevron-right" /></a>
                     <ul>
-                      <li><a href="/">Math</a></li>
+                      <li><a href="https://drive.google.com/file/d/1qG0ogL81uDxaaMXfjgO2XRdOIjRkuSy-/view?usp=sharing">Math</a></li>
                       <li><a href="/">Science</a></li>
                     </ul>
                   </li>
